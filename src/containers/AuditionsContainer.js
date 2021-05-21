f605989a3c704a65aa13a0470f721bb10d36bbd1
agilles1 <adam.g.tpt@gmail.com>
2021-05-20 22:44:33 -0400

    create audition container

---
+++ src/containers/AuditionsContainer.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import AuditionForm from '../components/AuditionForm.js'
+
+class AuditionsContainer extends React.Component {
+
+    render(){
+        return (
+            <div>
+                <AuditionForm />
+            </div>
+        )
+    }
+}
+
+export default AuditionsContainer

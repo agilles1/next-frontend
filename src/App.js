fddf9e68ecacccd6bfa9b101b642c5f681123ab8
agilles1 <adam.g.tpt@gmail.com>
2021-05-20 21:15:25 -0400

    Test connection to backend

--- src/App.js
+++ src/App.js
@@ -2,6 +2,13 @@ import React from 'react'
 
 class App extends React.Component {
 
+  // componentDidMount(){
+  //   fetch('http://localhost:3000/api/v1/auditions')
+  //   .then(res => res.json())
+  //   .then(data => console.log(data[0].instrument))
+  // }
+
+
   render() {
     return (
       <div className="App">

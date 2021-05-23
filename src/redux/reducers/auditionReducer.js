2a968d7226e9a57e3768703ed5249bed91d544d2
agilles1 <adam.g.tpt@gmail.com>
2021-05-23 11:07:25 -0400

    add audition reducer

--- src/redux/reducers/auditionReducer.js
+++ src/redux/reducers/auditionReducer.js
@@ -1,1 +1,14 @@
 //repsonsible for updating the store with auditon info
+
+const auditionReducer = (state = {}, action) => {
+    switch(action.type){
+        case "CREATE_AUDITION_SUCCESS":
+            return action.payload;
+        case "GET_AUDITION_SUCCESS":
+            return action.payload;
+        default:
+            return state;
+    }
+}
+
+export default auditionReducer

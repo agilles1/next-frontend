01d44987634ee4046acf9d552679c914a8e02279
agilles1 <adam.g.tpt@gmail.com>
2021-05-23 14:37:24 -0400

    move candidate container

--- src/components/Audition.js
+++ src/components/Audition.js
@@ -1,29 +1,23 @@
 import React from 'react'
 import { connect } from 'react-redux'
-import { getAudition } from "../redux/actions/auditionActions"
 import Moment from 'moment'
-import CandidatesContainer from '../containers/CandidatesContainer'
+
 
 class Audition extends React.Component {
-    
-    componentDidMount(){
-        this.props.getAudition(this.props.match.params.id)
-    }
-    
+        
     render(){
 
-        let audition = this.props.audition.audition
+        let audition = this.props.audition
 
         return(
             <div>
                 <h3>{audition.instrument} | {Moment(audition.date).format('M-DD-YY HH:MM')}</h3>
-                <CandidatesContainer />
             </div>
         )
 
     }
 
-
 }
 
-export default connect(null, { getAudition })(Audition)
+
+export default Audition

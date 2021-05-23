5d358ac67ad920d53ead7faad425845302d86b0d
agilles1 <adam.g.tpt@gmail.com>
2021-05-23 12:10:39 -0400

    create CandiadteForm fields

--- src/components/CandidateForm.js
+++ src/components/CandidateForm.js
@@ -4,12 +4,29 @@ import { createNewCandidate } from "../redux/actions/candidateActions.js"
 
 class CandidateForm extends React.Component {
     state ={
-        
+        name: "",
+        email: "", 
+        phone: "",
     }
+
+    handleChange = (e) => {
+        this.setState(
+           {[e.target.name]: e.target.value}
+        )
+    }
+
     render(){
         return(
             <div>
-                hi
+                <form onSubmit={this.handleSubmit}>
+                    <input type="text" name="name" placeholder="Name" onChange={this.handleChange}/>
+                    <br/>
+                    <input type="text" name="email" placeholder="Email" onChange={this.handleChange}/>
+                    <br/>
+                    <input type="text" name="phone" placeholder="Phone" onChange={this.handleChange}/>
+                    <br/>
+                    <button type="submit">Add</button>
+                </form>
             </div>
         )
     }

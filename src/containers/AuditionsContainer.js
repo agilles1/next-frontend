0bb9a8a01d00945e6da3fbbb7c2ba7cd6c2cb5d5
agilles1 <adam.g.tpt@gmail.com>
2021-05-23 11:06:10 -0400

    Update routes and date format with Moment

--- src/containers/AuditionsContainer.js
+++ src/containers/AuditionsContainer.js
@@ -1,15 +1,26 @@
 import React from 'react'
+import { Route} from 'react-router'
+import { connect } from "react-redux";
 import AuditionForm from '../components/AuditionForm.js'
+import Audition from '../components/Audition.js'
 
 class AuditionsContainer extends React.Component {
 
     render(){
         return (
             <div>
-                <AuditionForm />
+                 <Route exact path='/auditions/new' component={AuditionForm} />
+                 <Route path='/auditions/:id' render={(routerProps) => <Audition {...routerProps} audition={this.props.audition}/>} />
             </div>
         )
-    }
+    } 
 }
 
-export default AuditionsContainer
+const mapStateToProps = (state) => {
+    
+    return {
+        audition: state
+    }
+};
+
+export default connect(mapStateToProps)(AuditionsContainer)

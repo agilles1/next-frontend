b9bdd1f4eba30d70237e8650e93f2fccc7400c60
agilles1 <adam.g.tpt@gmail.com>
2021-05-23 11:06:53 -0400

    add audition actions

--- src/redux/actions/auditionActions.js
+++ src/redux/actions/auditionActions.js
@@ -1,8 +1,33 @@
-export const getAudition = () => {
-    
+export const getAudition = (auditionId) => {
+    return(dispatch) => {
+        fetch(`http://localhost:3000/api/v1/auditions/${auditionId}`)
+        .then(res => res.json())
+        .then(data =>  dispatch({ type: "GET_AUDITION_SUCCESS", payload: data}))
+        .then(data => console.log(data.payload))
+    }
 }
 
-export const createAudition = () => {
+export const createAudition = (newAuditionData, history) => {
+    return (dispatch) => {
+            fetch(`http://localhost:3000/api/v1/auditions`, {
+                method: "POST",
+                headers: {
+                    Accepts: "application/json",
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify({audition: newAuditionData})
+            })
+            .then((res) => {
+                if (res.ok) {
+                return res.json();
+                } else {
+                throw new Error(res.statusText);
+                }
+            })
+            .then((data) => {
+                dispatch({ type: "CREATE_AUDITION_SUCCESS", payload: data});
+                history.push(`/auditions/${data.id}`)
+            })
 
+        }
 }
-

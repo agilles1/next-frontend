8e17e17333febdc3b84641b0e26f824da8b5d809
agilles1 <adam.g.tpt@gmail.com>
2021-05-23 11:05:41 -0400

    add Cadndiate Component

---
+++ src/components/Candidate.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+class Candidate extends React.Component{
+
+    render(){
+        return(
+            <div>
+
+            </div>
+        )
+    }
+}
+
+export default Candidate

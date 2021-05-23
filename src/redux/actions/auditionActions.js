be956722467c348a497d774b3eb1077f2155f7ba
agilles1 <adam.g.tpt@gmail.com>
2021-05-23 14:38:35 -0400

    add candidate actions

--- src/redux/actions/auditionActions.js
+++ src/redux/actions/auditionActions.js
@@ -30,3 +30,33 @@ export const createAudition = (newAuditionData, history) => {
 
         }
 }
+
+export const createNewCandidate = (newCandidateData) => {
+    return (dispatch) => {
+        fetch(`http://localhost:3000/api/v1/auditions/${newCandidateData.audition_id}/candidates`, {
+            method: "POST",
+            headers: {
+                Accepts: "application/json",
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({candidate: newCandidateData})
+        })
+        .then((res) => {
+            if (res.ok) {
+              return res.json();
+            } else {
+              throw new Error(res.statusText);
+            }
+          })
+          .then((data) => {
+              dispatch({ type: "CREATE_CANDIDATE_SUCCESS", payload: data})
+          })
+    }
+}
+
+export const getCandidates = (auditionId) => {
+  debugger
+  return (dispatch) => {
+
+  }
+}

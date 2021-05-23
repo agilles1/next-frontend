ff2a4072d6a9f3c3d73efb63e94adf4b5175c417
agilles1 <adam.g.tpt@gmail.com>
2021-05-23 12:10:14 -0400

    Add candidate rendering

--- src/components/Candidate.js
+++ src/components/Candidate.js
@@ -3,9 +3,10 @@ import React from 'react'
 class Candidate extends React.Component{
 
     render(){
+        let candidate = this.props.candidate 
         return(
             <div>
-
+                {candidate.name} - {candidate.number} - {candidate.room.name}
             </div>
         )
     }

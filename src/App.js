1c6d082ed6ee70227263d3d39ffd762de24e59c9
agilles1 <adam.g.tpt@gmail.com>
2021-05-23 14:37:03 -0400

    Add route to audition container

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import AuditionsContainer from './containers/AuditionsContainer'
+import { Route} from 'react-router'
 
 class App extends React.Component {
 
@@ -13,7 +14,7 @@ class App extends React.Component {
   render() {
     return (
       <div className="App">
-        <AuditionsContainer />
+        <Route path='/auditions/:id' render={(routerProps) => <AuditionsContainer {...routerProps}/>} />
       </div>
     );
   }

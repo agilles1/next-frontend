1233fc3fda45d58bac96a07cd9a1c4a65e86abea
agilles1 <adam.g.tpt@gmail.com>
2021-05-23 14:37:50 -0400

    Update audition cont

--- src/containers/AuditionsContainer.js
+++ src/containers/AuditionsContainer.js
@@ -3,14 +3,21 @@ import { Route} from 'react-router'
 import { connect } from "react-redux";
 import AuditionForm from '../components/AuditionForm.js'
 import Audition from '../components/Audition.js'
+import { getAudition } from "../redux/actions/auditionActions"
+import CandidatesContainer from '../containers/CandidatesContainer'
 
 class AuditionsContainer extends React.Component {
+
+    componentDidMount(){
+        this.props.getAudition("1")
+    }
 
     render(){
         return (
             <div>
                  <Route exact path='/auditions/new' component={AuditionForm} />
-                 <Route path='/auditions/:id' render={(routerProps) => <Audition {...routerProps} audition={this.props.audition}/>} />
+                 <Audition audition={this.props.audition}/>
+                 <CandidatesContainer />
             </div>
         )
     } 
@@ -19,8 +26,8 @@ class AuditionsContainer extends React.Component {
 const mapStateToProps = (state) => {
     
     return {
-        audition: state
+        audition: state.audition
     }
 };
 
-export default connect(mapStateToProps)(AuditionsContainer)
+export default connect(mapStateToProps, { getAudition })(AuditionsContainer)

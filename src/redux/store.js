41a4d58d353032f18e9c2d72e16a4b107e9a3ab3
agilles1 <adam.g.tpt@gmail.com>
2021-05-25 21:55:42 -0400

    Add auditions to store

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,9 +1,11 @@
 import { createStore, applyMiddleware, compose, combineReducers } from 'redux'
 import thunk from 'redux-thunk'
 import auditionReducer from './reducers/auditionReducer'
+import auditionsReducer from './reducers/auditionsReducer'
 
 const rootReducer = combineReducers({
-    audition: auditionReducer
+    audition: auditionReducer, 
+    auditions: auditionsReducer
 })
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 

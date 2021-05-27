640ae767da18c7eb8772ffe69427acca181001a4
agilles1 <adam.g.tpt@gmail.com>
2021-05-26 21:06:30 -0400

    create auditionCard

---
+++ src/components/AuditionCard.js
@@ -0,0 +1,21 @@
+import React from 'react' 
+import { Card } from 'react-bootstrap'
+import Moment from 'moment'
+import { Link } from 'react-router-dom'
+
+function AuditionCard({ audition }){
+    return (
+        <Link key={audition.id} to={`/auditions/${audition.id}`}>
+            <Card  bg={"success"} text={"light"}>
+                <Card.Header>{Moment(audition.date).format('MMMM DD, YYYY | h:mma')}</Card.Header>
+                <Card.Body>
+                    Instrument: {audition.instrument}
+                    <br/>
+                    Candidates: {audition.candidates.length}
+                </Card.Body>
+            </Card>
+        </Link>
+    )
+}
+
+export default AuditionCard
--- src/components/Auditions.js
+++ src/components/Auditions.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import { connect } from 'react-redux'
-import { Card, Button } from 'react-bootstrap'
+import { Button, CardColumns} from 'react-bootstrap'
 import { getAuditions } from '../redux/actions/auditionsActions.js'
-import Moment from 'moment'
 import { Link } from 'react-router-dom'
+import AuditionCard from './AuditionCard'
 
 class Auditions extends React.Component {
     componentDidMount(){
@@ -11,26 +11,20 @@ class Auditions extends React.Component {
     }
 
     render(){
-        let auditionCards = this.props.auditions.map(audition => 
-            <Link to={`/auditions/${audition.id}`}>
-                <Card bg={"success"} text={"light"}>
-                    <Card.Header>{Moment(audition.date).format('MMMM DD, YYYY | h:mma')}</Card.Header>
-                    <Card.Body>
-                        Instrument: {audition.instrument}
-                        <br/>
-                        Candidates: {audition.candidates.length}
-                    </Card.Body>
-                </Card>
-            </Link>)
+        
+        let auditionCards = this.props.auditions.map(audition => <AuditionCard audition={audition}/>)
 
-            
-           
         return(
-            <>
-                <Button variant="success">Add New Audition</Button>
-                
-                {auditionCards}
-            </>
+            <div>
+                <Link to="/auditions/new">
+                    <Button variant="success">Add New Audition</Button>
+                </Link>
+                <br/>
+                <br/>
+                <CardColumns>
+                    {auditionCards}
+                </CardColumns>
+            </div>
         )
     }
 }

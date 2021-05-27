8d28a2f7b6d81c26f9b7a02affdafdbafcdcf0b4
agilles1 <adam.g.tpt@gmail.com>
2021-05-26 21:06:49 -0400

    Update to bootstrap form

--- src/components/AuditionForm.js
+++ src/components/AuditionForm.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { createAudition } from "../redux/actions/auditionActions";
 import { connect } from "react-redux";
-import { Button } from 'react-bootstrap'
+import { Button, Form } from 'react-bootstrap'
 
 class AuditionForm extends React.Component {
     state = {
@@ -13,6 +13,7 @@ class AuditionForm extends React.Component {
 
     handleSubmit = (e) => {
         e.preventDefault();
+        debugger
         this.props.createAudition(this.state, this.props.history)
     }
 
@@ -23,28 +24,56 @@ class AuditionForm extends React.Component {
     }
 
     render() {
+     
         return (
             
             <div>
                 <h3>Start New Audition</h3>
-                <form onSubmit={this.handleSubmit}>
-                    <label>Date</label>
-                    <input type="date" name="date" onChange={this.handleChange}/>
-                    <label>Start Time</label>
-                    <input type="time" name="time" onChange={this.handleChange}/>
-                   
-                    <hr/>
-                    <label>Instrument</label>
-                    <br/>
-                    <input type="text" name="instrument" value={this.state.instrument} onChange={this.handleChange}/>
-                    <hr/>
-                    <label>Individual Warm-Up Rooms (listed in fill order)</label>
-                    <br/>
-                    <input type="text" name="rooms" value={this.state.rooms} onChange={this.handleChange}/>
-                    <hr/>
-                    <Button variant="success" type="submit">Start</Button>
-                    
-                </form>
+                <Form onSubmit={this.handleSubmit}>
+                    <Form.Group>
+                        <Form.Label>Date</Form.Label>
+                        <Form.Control type="date" name="date" onChange={this.handleChange} />
+                    </Form.Group>
+
+                    <Form.Group>
+                        <Form.Label>Time</Form.Label>
+                        <Form.Control type="time" name="time" onChange={this.handleChange} />
+                    </Form.Group>
+
+                    <Form.Group>
+                        <Form.Label>Instrument</Form.Label>
+                        <Form.Control as="select" name="instrument" value={this.state.instrument} onChange={this.handleChange}>
+                            <option>-</option>
+                            <option>Flute</option>
+                            <option>Oboe</option>
+                            <option>Clarinet</option>
+                            <option>Bassoon</option>
+                            <option>Horn</option>
+                            <option>Trumpet</option>
+                            <option>Trombone</option>
+                            <option>Tuba</option>
+                            <option>Percussion</option>
+                            <option>Timpani</option>
+                            <option>Keyboard</option>
+                            <option>Violin 1</option>
+                            <option>Violin 2</option>
+                            <option>Viola</option>
+                            <option>Cello</option>
+                            <option>Bass</option>
+                            <option>Other</option>
+                        </Form.Control>
+                    </Form.Group>
+
+                    <Form.Group>
+                        <Form.Label>Warm-Up Rooms</Form.Label>
+                        <Form.Control type="text" name="rooms" value={this.state.rooms} onChange={this.handleChange}/>
+                        <Form.Text>Listed in fill order</Form.Text>
+                    </Form.Group>
+                  
+                    <Button variant="success" type="submit">
+                        Start
+                    </Button>
+                </Form>
             </div>
         )
     }
